4690e60d7252f96fb16a7aa165983e8c79f30fcf
Alex King <calicoalix@gmail.com>
2022-06-15 20:58:25 +1000

    linked stylesheet

--- js/connect.js
+++ js/connect.js
@@ -1,20 +1,3 @@
-console.log(`
-[FOR BEST EXPERIENCE, MAKE CONSOLE AS LARGE AS POSSIBLE]
-
-
-
-
-WELCOME TO 
-\r\n\r\n   ____   U  ___ u  _   _     _   _   U _____ u   ____   _____       _  _    \r\nU \/\"___|   \\\/\"_ \\\/ | \\ |\"|   | \\ |\"|  \\| ___\"|\/U \/\"___| |_ \" _|     | ||\"|   \r\n\\| | u     | | | |<|  \\| |> <|  \\| |>  |  _|\"  \\| | u     | |       | || |_  \r\n | |\/__.-,_| |_| |U| |\\  |u U| |\\  |u  | |___   | |\/__   \/| |\\      |__   _| \r\n  \\____|\\_)-\\___\/  |_| \\_|   |_| \\_|   |_____|   \\____| u |_|U        \/|_|\\  \r\n _\/\/ \\\\      \\\\    ||   \\\\,-.||   \\\\,-.<<   >>  _\/\/ \\\\  _\/\/ \\\\_      u_|||_u \r\n(__)(__)    (__)   (_\")  (_\/ (_\")  (_\/(__) (__)(__)(__)(__) (__)     (__)__) \r\n\r\n
-
-`)
-
-const help = () => {
-    console.log('1. Place your piece by calling function play(column) with the column you select as your parameter.')
-    console.log('2. Toggle between single and multiplayer by calling pcPlayer(true) or pcPlayer(false).')
-    console.log('3. If you need help, use help().')
-} // help()
-help()
 
 const params = {
     height: 6,
